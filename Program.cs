29d04f58d048cedda5dcf1c0431b39ac648ab9bf
Trần Mỹ Phú Lâm <87407895+PhuLamCoder@users.noreply.github.com>
2022-12-02 22:17:39 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            if (args.Length < 2)
+                DownloadUtil.SingleConnection(args[0]);
+            else
+            {
+                List<string> ls = new List<string>();
+                foreach (var item in args)
+                {
+                    ls.Add(item);
+                }
+                DownloadUtil.MultiConnection(ls);
+            }
+
+            Console.WriteLine("Downloading process has finnished! Press any key to exit!");
+            Console.ReadLine();
+        }
+    }
+}
